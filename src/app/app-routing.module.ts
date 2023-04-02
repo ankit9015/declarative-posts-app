1060f9430c4d6c0e43a45685f914a9c7194871fd
Ankit Joshi <ankitoshi9015@gmail.com>
2023-04-02 16:31:21 +0530

    feat: counter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsComponent } from './pages/posts/posts.component';
+import { CounterComponent } from './counter/counter/counter.component';
 
-const routes: Routes = [{ path: '', component: PostsComponent }];
+const routes: Routes = [{ path: '', component: CounterComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,28 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PostsComponent } from './pages/posts/posts.component';
 import { CounterComponent } from './counter/counter/counter.component';
-import { CounterOutputComponent } from './couter/counter-output/counter-output.component';
-import { CounterButtonsComponent } from './couter/counter-buttons/counter-buttons.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter/state/counter.reducer';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, PostsComponent, CounterComponent, CounterOutputComponent, CounterButtonsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PostsComponent,
+    CounterComponent,
+    CounterOutputComponent,
+    CounterButtonsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({
+      counter: counterReducer,
+    }),
+  ],
   providers: [],
 
   bootstrap: [AppComponent],
